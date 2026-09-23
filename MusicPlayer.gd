9450c949ae59ee2b41f0ec3b7d5a025b254892d3
ringo071955 <rintokundayo@gmail.com>
2026-09-23 21:49:36 +0900

    add

--- MusicPlayer.gd
+++ MusicPlayer.gd
@@ -5,11 +5,18 @@ extends Node
 
 func _ready():
 	add_child(player)
-	player.stream = preload("res://SE・BGM/BGM/audiostock_1086176.mp3")
-	player.volume_db = -5
+	var bgm = preload("res://SE・BGM/BGM/audiostock_1086176.mp3")
+	if bgm is AudioStreamMP3:
+		bgm.loop = true
+	player.stream = bgm
+	# 元の音量 -5.0 dB をリニア値で1/3に調整 (-5.0 - 9.54 dB ≈ -14.5 dB)
+	player.volume_db = -14.5
+	player.finished.connect(func(): player.play())
 	player.play()
 
 func play_track(stream: AudioStream, fade_in: bool = false):
+	if stream is AudioStreamMP3:
+		stream.loop = true
 	player.stream = stream
 	player.play()
 
